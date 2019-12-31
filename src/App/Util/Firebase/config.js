459289ff3aeba5883ad01f7f7c3a1f1fa59f1cab
Mig Tam Rod <migtam628@gmail.com>
2019-12-30 21:24:07 -0500

    Update config.js

--- src/App/Util/Firebase/config.js
+++ src/App/Util/Firebase/config.js
@@ -1,10 +1,10 @@
 export const config = {
-  apiKey: "AIzaSyA-td1_ALkP1AzGJdSh8PDsj8O4u5s4vbk",
-  authDomain: "b4ushop-112119.firebaseapp.com",
-  databaseURL: "https://b4ushop-112119.firebaseio.com",
-  projectId: "b4ushop-112119",
-  storageBucket: "b4ushop-112119.appspot.com",
-  messagingSenderId: "871025946714",
-  appId: "1:871025946714:web:480aa5be08437837b62863",
-  measurementId: "G-HV6Q5FJG2E"
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  databaseURL: "DATABASE_URL",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "MEASSUREMENT_ID"
 };
